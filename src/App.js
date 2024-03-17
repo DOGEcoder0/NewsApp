fb894a525bdf98fbf37ab9243f0dfa8b73c28e64
Dharmendra mahala <dharmendramahalagci2003@gmail.com>
2024-03-17 20:21:48 +0530

    layout of webpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-
-import React, { Component } from 'react'
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./componants/Navbar";
+import News from "./componants/News";
 
 export default class App extends Component {
-  x = "dharmendra";
+  
   render() {
-    return (
-      <div>
-        hello my first class based componants and {this.x}
-      </div>
-    )
+    return <div>
+      <Navbar/>
+      <News/>
+    </div>;
   }
 }
---
+++ src/componants/News.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export class News extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+    };
+  }
+
+  async componentDidMount(){
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=fe6b2d60a34741568494ba0de08ab273"
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    this.setState({articles:parsedData.articles})
+  }
+
+
+  render() {
+    return (
+      <div className="container my-3">
+        <h1>NewsMonkey - Top Headlines</h1>
+        <div className="row">
+          {this.state.articles.map((element)=>{
+           return  <div className="col-md-4" key={element.url}>
+            <NewsItem
+              title={element.title}
+              description={element.description}
+              imageUrl={element.urlToImage}
+              newsUrl={element.url}
+            />
+          </div>
+          })}
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
